e53c71b3b99b326e8e2394cf15a3acaf8a92008d
MoeHesh123 <megamind850@gmail.com>
2024-04-29 13:37:21 +0200

    asd

--- DSPhase2/ADT/Node.h
+++ DSPhase2/ADT/Node.h
@@ -1,1 +1,51 @@
-#pragma once
+#ifndef _NODE
+#define _NODE
+
+template <class T>
+class Node
+{
+
+    T item;
+    Node<T>* next;
+
+public:
+
+    Node()
+    {
+        next = nullptr;
+    }
+
+    Node(const T& r_Item)
+    {
+        item = r_Item;
+        next = nullptr;
+    }
+
+    Node(const T& r_Item, Node<T>* nextNodePtr)
+    {
+        item = r_Item;
+        next = nextNodePtr;
+    }
+
+    void setItem(const T& r_Item)
+    {
+        item = r_Item;
+    }
+
+    void setNext(Node<T>* nextNodePtr)
+    {
+        next = nextNodePtr;
+    }
+
+    T getItem() const
+    {
+        return item;
+    }
+
+    Node<T>* getNext() const
+    {
+        return next;
+    }
+
+};
+#endif
--- DSPhase2/ADT/StackADT.h
+++ DSPhase2/ADT/StackADT.h
@@ -1,1 +1,23 @@
-#pragma once
+#include"Node.h"
+
+#ifndef STACK_ADT_
+#define STACK_ADT_
+
+template<typename T>
+class StackADT
+{
+
+public:
+
+	virtual bool isEmpty() const = 0;
+
+	virtual bool push(const T& newEntry) = 0;
+
+	virtual bool pop(T& TopEntry) = 0;
+
+	virtual bool peek(T& TopEntry) const = 0;
+
+	virtual ~StackADT() { }
+
+};
+#endif
