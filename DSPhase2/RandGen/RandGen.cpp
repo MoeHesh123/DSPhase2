09f8fbaaf21ef00d1d380207596eb179314325bf
Abdelrahman Kamal <abdelrahmankamal2004@gmail.com>
2024-04-30 09:52:18 +0300

    A7a

--- DSPhase2/RandGen/RandGen.cpp
+++ DSPhase2/RandGen/RandGen.cpp
@@ -10,9 +10,9 @@ void RandGen::Generate(Game* game, EarthArmy* EA, AlienArmy* ALA,int tj)
 		for (int i = 0; i < game->in.numOfUnits; i++)
 		{
 			int B = 1 + rand() % 100;
-			int H = game->in.minHealthEarth + (rand() % game->in.maxHealthEarth - game->in.minHealthEarth);
-			int P = game->in.minPowerEarth + (rand() % game->in.maxPowerEarth - game->in.minPowerEarth);
-			int C = game->in.minCapacityEarth + (rand() % game->in.maxCapacityEarth - game->in.minCapacityEarth);
+			int H = game->in.minHealthEarth + (rand() % (game->in.maxHealthEarth - game->in.minHealthEarth));
+			int P = game->in.minPowerEarth + (rand() % (game->in.maxPowerEarth - game->in.minPowerEarth));
+			int C = game->in.minCapacityEarth + (rand() % (game->in.maxCapacityEarth - game->in.minCapacityEarth));
 			if (B <= game->in.ES)
 			{
 				EA->addES(tj, H, P, C);
@@ -29,9 +29,9 @@ void RandGen::Generate(Game* game, EarthArmy* EA, AlienArmy* ALA,int tj)
 		for (int i = 0; i < game->in.numOfUnits; i++)
 		{
 			int Z = 1 + rand() % 100;
-			int H = game->in.minHealthAlien + (rand() % game->in.maxHealthAlien - game->in.minHealthAlien);
-			int P = game->in.minPowerAlien + (rand() % game->in.maxPowerAlien - game->in.minPowerAlien);
-			int C = game->in.minCapacityAlien + (rand() % game->in.maxCapacityAlien - game->in.minCapacityAlien);
+			int H = game->in.minHealthAlien + (rand() % (game->in.maxHealthAlien - game->in.minHealthAlien));
+			int P = game->in.minPowerAlien + (rand() % (game->in.maxPowerAlien - game->in.minPowerAlien));
+			int C = game->in.minCapacityAlien + (rand() % (game->in.maxCapacityAlien - game->in.minCapacityAlien));
 			if (Z <= game->in.AS)
 			{
 				ALA->addAS(tj, H, P, C);
