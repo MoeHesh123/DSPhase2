b99925df7b89f5a9726b0510427f2b7e68904a4e
lujinashawki@gmail.com <lujinashawki@gmail.com>
2024-05-07 01:39:50 +0300

    earthtank

--- DSPhase2/Unit/EarthTank.cpp
+++ DSPhase2/Unit/EarthTank.cpp
@@ -18,69 +18,31 @@ void EarthTank::Attack(Game* game)
 	AlienMonster* AM = nullptr;
 	EarthTank* ET = nullptr;
 	EarthSoldier* ES = nullptr;
-
-	int attackCount = 0;
 	int attackCountAM = 0;
 	int attackCountAS = 0;
 
 	game->GetEA()->peekET(ET);
+	if (ET)
+	{
+		game->addETAttack(ET);
+	}
 	if (!(game->GetEA()->isEmpty_ET()))
 	{
 		if ((game->GetEA()->getEScount()) < (0.3 * (game->GetAA()->getAScount())))
 		{
 			while ((game->GetEA()->getEScount()) < (0.8 * game->GetAA()->getAScount()))
 			{
-				if (ET->GetAttackCapacity() % 2 == 0)
-				{
-					attackCount = (ET->GetAttackCapacity() / 2);
-					for (int i = 0; i < attackCount; i++)
-					{
-						if (!(game->GetAA()->isEmpty_AS()))
-						{
-							game->GetAA()->removeAS(AS);
-							AS->SetHealth(AS->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AS->GetHealth()));
-							if (AS->GetTa() == 0)
-							{
-								AS->SetTa(game->GetTimeStep());
-							}
-							if (AS->GetHealth() <= 0)
-							{
-								game->AddToKilled(AS);
-								AS->SetTd(game->GetTimeStep());
-							}
-							else
-							{
-								tempAS.enqueue(AS);
-							}
-						}
-						if (!(game->GetAA()->isEmpty_AM()))
-						{
-							game->GetAA()->removeAM(AM);
-							AM->SetHealth(AM->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AM->GetHealth()));
-							if (AM->GetTa() == 0)
-							{
-								AM->SetTa(game->GetTimeStep());
-							}
-							if (AM->GetHealth() <= 0)
-							{
-								game->AddToKilled(AM);
-								AM->SetTd(game->GetTimeStep());
-							}
-							else
-							{
-								tempAM.enqueue(AM);
-							}
-						}
-					}
-				}
-				else
-				{
 					attackCountAS = ((ET->GetAttackCapacity()) / 2);
 					attackCountAM = (ET->GetAttackCapacity() - attackCountAS);
 					for (int i = 0; i < attackCountAS; i++)
 					{
 						if (!(game->GetAA()->isEmpty_AS()))
 						{
+							game->GetAA()->peekAS(AS);
+							if (AS)
+							{
+								game->addETAttack(AS); 
+							}
 							game->GetAA()->removeAS(AS);
 							AS->SetHealth(AS->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AS->GetHealth()));
 							if (AS->GetTa() == 0)
@@ -95,6 +57,32 @@ void EarthTank::Attack(Game* game)
 							else
 							{
 								tempAS.enqueue(AS);
+							}
+						}
+						else
+						{
+							if (!(game->GetAA()->isEmpty_AM()))
+							{
+								game->GetAA()->peekAM(AM);
+								if (AM)
+								{
+									game->addETAttack(AM);
+								}
+								game->GetAA()->removeAM(AM);
+								AM->SetHealth(AM->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AM->GetHealth()));
+								if (AM->GetTa() == 0)
+								{
+									AM->SetTa(game->GetTimeStep());
+								}
+								if (AM->GetHealth() <= 0)
+								{
+									game->AddToKilled(AM);
+									AM->SetTd(game->GetTimeStep());
+								}
+								else
+								{
+									tempAM.enqueue(AM);
+								}
 							}
 						}
 					}
@@ -102,6 +90,11 @@ void EarthTank::Attack(Game* game)
 					{
 						if (!(game->GetAA()->isEmpty_AM()))
 						{
+							game->GetAA()->peekAM(AM); 
+							if (AM) 
+							{
+								game->addETAttack(AM); 
+							}
 							game->GetAA()->removeAM(AM);
 							AM->SetHealth(AM->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AM->GetHealth()));
 							if (AM->GetTa() == 0)
@@ -118,8 +111,34 @@ void EarthTank::Attack(Game* game)
 								tempAM.enqueue(AM);
 							}
 						}
+						else
+						{
+							if (!(game->GetAA()->isEmpty_AS())) 
+							{
+								game->GetAA()->peekAS(AS); 
+								if (AS) 
+								{
+									game->addETAttack(AS); 
+								}
+								game->GetAA()->removeAS(AS); 
+								AS->SetHealth(AS->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AS->GetHealth())); 
+								if (AS->GetTa() == 0) 
+								{
+									AS->SetTa(game->GetTimeStep()); 
+								}
+								if (AS->GetHealth() <= 0) 
+								{
+									game->AddToKilled(AS); 
+									AS->SetTd(game->GetTimeStep()); 
+								}
+								else
+								{
+									tempAS.enqueue(AS); 
+								}
+							}
+						}
 					}
-				}
+				
 				break;
 			}
 		}
@@ -129,6 +148,11 @@ void EarthTank::Attack(Game* game)
 			{
 				if (!(game->GetAA()->isEmpty_AM()))
 				{
+					game->GetAA()->peekAM(AM); 
+					if (AM)
+					{
+						game->addETAttack(AM); 
+					}
 					game->GetAA()->removeAM(AM);
 					AM->SetHealth(AM->GetHealth() - (ET->GetPower() * ((ET->GetHealth()) / 100)) / sqrt(AM->GetHealth()));
 					if (AM->GetTa() == 0)
